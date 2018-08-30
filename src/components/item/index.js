4b99e08798e00748d2ff0d9905d5162510f8a71e
Andrew Royce <andrew.w.royce@gmail.com>
2018-08-30 10:27:25 -0400

    Removed ripple

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,49 +1,39 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import { Button } from 'react-native-material-buttons';
+import { View, TouchableNativeFeedback } from 'react-native';
 
 import styles from './styles';
 
 export default class DropdownItem extends PureComponent {
-  static defaultProps = {
-    color: 'transparent',
-    disabledColor: 'transparent',
-    rippleContainerBorderRadius: 0,
-    shadeBorderRadius: 0,
-  };
+    static defaultProps = {
+        color: 'transparent',
+        disabledColor: 'transparent',
+        shadeBorderRadius: 0,
+    };
 
-  static propTypes = {
-    ...Button.propTypes,
+    static propTypes = {
+        index: PropTypes.number.isRequired,
+    };
 
-    index: PropTypes.number.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.onPress = this.onPress.bind(this);
-  }
-
-  onPress() {
-    let { onPress, index } = this.props;
-
-    if ('function' === typeof onPress) {
-      onPress(index);
+    constructor(props) {
+        super(props);
     }
-  }
 
-  render() {
-    let { children, style, index, ...props } = this.props;
+    onPress = () => {
+        let { onPress, index } = this.props;
 
-    return (
-      <Button
-        {...props}
+        if ('function' === typeof onPress) {
+            onPress(index);
+        }
+    };
 
-        style={[styles.container, style]}
-        onPress={this.onPress}
-      >
-        {children}
-      </Button>
-    );
-  }
+    render() {
+        let { children, style, index, ...props } = this.props;
+
+        return (
+            <TouchableNativeFeedback {...props} onPress={this.onPress}>
+                <View style={[styles.container, style]}>{children}</View>
+            </TouchableNativeFeedback>
+        );
+    }
 }
