fc52fe3e25acd99b2e52efa861d023e95be9bdd8
ruslandjent <sayfulinruslan.djnt@gmail.com>
2020-05-18 11:03:21 +0500

    textField dependency changed

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -13,7 +13,7 @@ import {
   I18nManager,
 } from 'react-native';
 import Ripple from 'react-native-material-ripple';
-import { TextField } from 'react-native-material-textfield';
+import { TextField } from '@softmedialab/react-native-material-textfield';
 
 import DropdownItem from '../item';
 import styles from './styles';
